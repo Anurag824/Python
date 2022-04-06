e1e33c5faa8e3192e443577f610cd986e4eea002
Anurag Kushwaha <anurag.kush.ak@gmail.com>
2022-04-06 23:39:43 +0530

    PYnative

---
+++ exercise1.py
@@ -0,0 +1,7 @@
+def AddorMultiply(a,b):
+    print(a*b if a*b<=1000 else a+b)
+#PYnative excercise 1
+#Calculate multiplication if product is less than 1000 else add
+a=int(input())
+b=int(input())
+AddorMultiply(a,b)
